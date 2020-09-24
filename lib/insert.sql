aa1998a93b40bd14b491a4ed9625fd3fe571fe52
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-24 04:23:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Mad", "1")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "Male", "Brown", "Mad", "1")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "Male", "Brown", "Sad", "1")
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "Female", "Brown", "Mad", "1")
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "Female", "Brown", "Mad", "1")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "Male", "Brown", "Mad", "1")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "Male", "Brown", "Happy", "1")
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "Female", "Brown", "Mad", "1")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "Male", "Brown", "Mad", "1")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "Male", "Brown", "Sad", "1")
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, "Female", "Brown", "Mad", "1")
