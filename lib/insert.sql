c0bd1742103bfc8caf70d981d8e773986c8a3475
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-24 04:21:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,7 +6,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabith
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "Male", "Brown", "Mad", "1")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Mad", "1")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "Male", "Brown", "Mad", "1")
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Mad", "1")
 
