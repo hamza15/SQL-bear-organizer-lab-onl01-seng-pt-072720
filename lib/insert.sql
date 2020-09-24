d56f66490cf7be44253b7ceef5c9b75072e82e11
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-24 04:21:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Mad", "1")
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Mad", "1")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "Male", "Brown", "Mad", "1")
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Brown", "Mad", "1")
 
