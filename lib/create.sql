b555f10af43f62d1a0e86f273bf9f3e9f9eecde3
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-24 04:13:08 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bears (id PRIMARY KEY INTEGER, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive )
+CREATE TABLE bears (id PRIMARY KEY INTEGER, name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, alive INTEGER)
